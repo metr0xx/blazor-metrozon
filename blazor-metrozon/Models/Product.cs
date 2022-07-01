d47fe91f00f4d9ac6f26ca71a43a543bc4080697
metr0xx <mishgan0702@gmail.com>
2022-07-02 00:49:30 +0300

    Добавьте файлы проекта.

---
+++ blazor-metrozon/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace blazor_metrozon.Models
+{
+    public class Product
+    {
+        public int product_id { get; set; }
+        public int seller_id { get; set; }
+        public int count { get; set; }
+        public int price { get; set; }
+        public double rating { get; set; }
+        public string name { get; set; }
+        public string category_id { get; set; }
+    }
+}
