70aa11c510c27940bc98d6d3c226465936b36e66
Mikle <82156668+metr0xx@users.noreply.github.com>
2022-09-07 18:39:42 +0300

    garbage cleaning

--- blazor-metrozon/Models/PostgresConnection.cs
+++ blazor-metrozon/Models/PostgresConnection.cs
@@ -64,7 +64,6 @@ namespace blazor_metrozon.Models
         }
         void AddProduct(string[] values)
             {
-
             }
 
             void DeleteProduct(int product_id)
@@ -75,29 +74,6 @@ namespace blazor_metrozon.Models
             {
 
             }
-
-
-
-            /*public static async void Connection()
-            {
-                String connectionString = "Server=localhost;Port=5432;User=postgres;Password=Vbifyz5558;Database=metrozonDB;";
-                await using var conn = new NpgsqlConnection(connectionString);
-                await conn.OpenAsync();
-                await using (var cmd = new NpgsqlCommand("INSERT INTO users (Name, Surname, Money, Phone, Email, Password) VALUES ('Bob', 'Semenov', 15000, '865384358', 'bs@bww.com', '123')", conn))
-                {
-                    cmd.Parameters.AddWithValue("Hello world");
-                    await cmd.ExecuteNonQueryAsync();
-                }
-                List<string> res = new List<string>();
-                await using (var cmd = new NpgsqlCommand("SELECT * FROM users", conn))
-                await using (var reader = await cmd.ExecuteReaderAsync())
-                {
-                    while (await reader.ReadAsync())
-                    {
-                        res.Add(reader.GetString(0));
-                    }
-                }
-            }*/
 
         }
 
